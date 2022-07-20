c17e439e2e251a04ff956c070b08f24d09bb81e4
David Moore <david@raindrop.com>
2022-07-20 15:42:58 -0700

    tableName

--- util.go
+++ util.go
@@ -89,8 +89,17 @@ func GcsUpload(ctx context.Context, client *storage.Client, r bufio.Reader, buck
 	return nil
 }
 
-func UpsertAttachmentGen(upsertAttachmentGQL string, attachmentName string, attachmentUuid string, attachmentGen int64, attachmentMime string, attachmentInstance int, attachmentUser string, contractId int, endpoint string, adminSecret string, bearer string) (int, string, error) {
-
+func UpsertAttachmentGen(tableName string, attachmentName string, attachmentUuid string, attachmentGen int64, attachmentMime string, attachmentInstance int, attachmentUser string, contractId int, endpoint string, adminSecret string, bearer string) (int, string, error) {
+	upsertAttachmentGQL = `mutation upsert_` + tableName + `_attachment($changes: [` + tableName + `_attachment_insert_input!]!) {
+		insert_` + tableName + `_attachment(objects: $changes, on_conflict: {constraint: ` + tableName + `_attachment_uuid_key, update_columns: [name, uuid, generation, mime_type, read_secret]}) {
+			affected_rows
+			returning {
+				id
+				uuid
+			}
+		}
+	}	
+`
 	if len(attachmentMime) < 1 {
 		attachmentMime = "application/octet-stream"
 	}
@@ -165,7 +174,7 @@ func GetGeneration(client *storage.Client, ctx context.Context, bucket string, n
 	}
 }
 
-func FileUpsert(file *bufio.Reader, instance int, fileName string, mime string, user string, uuidString string, contractID int, bucket string, upsertAttachmentGQL string, endpoint string, adminSecret string, bearer string) (int, string, int64, error) {
+func FileUpsert(file *bufio.Reader, instance int, fileName string, mime string, user string, uuidString string, contractID int, bucket string, tableName string, endpoint string, adminSecret string, bearer string) (int, string, int64, error) {
 	UUID := uuid.NewV4().String()
 
 	ustr := ""
@@ -213,7 +222,7 @@ func FileUpsert(file *bufio.Reader, instance int, fileName string, mime string, 
 		_ = mime
 	}
 
-	id, uuid, err := UpsertAttachmentGen(upsertAttachmentGQL, fileName, ustr, gen, mime, instance, user, contractID, endpoint, adminSecret, bearer)
+	id, uuid, err := UpsertAttachmentGen(tableName, fileName, ustr, gen, mime, instance, user, contractID, endpoint, adminSecret, bearer)
 	if err != nil {
 		fmt.Println("could not upsert attachment gen")
 
